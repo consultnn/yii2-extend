eb5eae163408d25c43dad27deb44ba1811635f95
Maxim Mironyuk <serieznyi@gmail.com>
2015-07-06 16:59:08 +0300

    add btn group rotation

--- components/BootstrapActionColumn.php
+++ components/BootstrapActionColumn.php
@@ -8,6 +8,13 @@ use yii\helpers\Html;
 
 class BootstrapActionColumn extends ActionColumn
 {
+    const BTN_GROUP_TYPE_HORIZONTAL = 'btn-group';
+    const BTN_GROUP_TYPE_VERTICAL = 'btn-group-vertical';
+    /**
+     * @var string
+     */
+    public $btnGroupType = self::BTN_GROUP_TYPE_HORIZONTAL;
+
     /**
      * @inheritdoc
      */
@@ -66,6 +73,6 @@ class BootstrapActionColumn extends ActionColumn
             }
         }, $this->template);
 
-        return Html::tag('div', $result, ['class' => 'btn-group btn-group-sm']);
+        return Html::tag('div', $result, ['class' => $this->btnGroupType.' btn-group-sm']);
     }
 }
